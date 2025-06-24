17b820ee239299e7c80ea7d2e58f415102cd72b1
Nati Friedman <nati.freedman@gmail.com>
2025-06-25 02:04:22 +0300

    defining profile model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,21 @@
+from datetime import timezone, datetime
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
+
+class Profile(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    daily_cost: so.Mapped[float] = so.mapped_column(sa.Float(2))
+    daily_smokes: so.Mapped[int] = so.mapped_column(sa.Integer)
+
+    users: so.WriteOnlyMapped['User'] = so.relationship(
+        back_populates='user_profile')
+
+    def __repr__(self):
+        return '<Profile {}>'.format(self.users)
+
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -11,6 +24,12 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    quit_date: so.Mapped[datetime] = so.mapped_column(sa.DateTime)
+    profile_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Profile.id),
+                                                  index=True)
+
+    user_profile: so.Mapped['Profile'] = so.relationship(
+        back_populates='users')
 
     def __repr__(self):
         return '<{}>'.format(self.username)
